873abd917d7eeac51c2c597028dd1c203155c283
spencergrimm23 <63203208+spencergrimm23@users.noreply.github.com>
2020-05-28 14:17:00 -0600

    Add files via upload

---
+++ s_sudo.h
@@ -0,0 +1,60 @@
+#ifndef __S_SUDO_H
+#define __S_SUDO_H
+
+/* Purpose: Check if having the data in the cell (row, col) as 'value'
+ * violates Sudoku rules.
+ * Return value: 0 if violates the rules
+ * 				 1 if doesn't violate the rules 
+ * Assumes parameter value is never 0.
+ */ 
+int is_valid_cell(int row, int col, int value, int game[9][9]);
+
+/* Purpose: Check is the given game grid is a valid Sudoku grid. 
+ * Return value: 0 if not-valid grid
+ *				 1 if valid grid
+ * Assumes parameter value is never 0.
+ */
+ 
+int is_valid_grid(int game[9][9]);
+
+/* Purpose: Find the next cell after (r,c) that has a value 0 (empty cell). 
+ * This function only looks for such cells in forward direction.  
+ * Return value: None
+ * Special note: The row and column numbers of the next empty cell 
+ * are populated in the given parameters next_r and next_c. 
+ */
+ 
+void get_next_cell(int r, int c, int *next_r, int *next_c, int game[9][9]);
+
+/* Purpose: Backtracking based recursive function to solve a given Sudoku 
+ * puzzle. Each call to the function finds a possible value for cell at (r,c)
+ * and makes a recursive call to next cell with a 0.
+ *
+ * Parameters: r and c give the row and col of the cell, 
+ *             ggame is the puzzle, 
+ *             sol is the grid to return the solution,
+ *             num_sols is to keep track of number of solutions seen so far. 
+ *
+ * Return value: 0 if ggame cannot be solved, 
+ *               1 if unique solution, 
+ *               2 if multiple solutions are possible
+ * Special note: This solver does not find multipl solutions
+ */
+
+int recursive_solve(int r, int c, int ggame[9][9], int sol[9][9], int num_sols);
+
+/* Purpose: To print the given puzzle grid to stdout
+ */
+void print_grid(int ggrid[9][9]);
+
+/* Purpose: To copy ogame grid to ngame grid
+ */
+void copy_game(int ogame[9][9], int ngame[9][9]);
+
+/* Purpose: To read a Sudoku puzzle grid from stdin
+ */
+int read_game(int ggame[9][9]);
+
+/* Top level Sudoku solver */
+int sudo_solve(int ggame[9][9]);
+#endif
